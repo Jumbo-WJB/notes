9f4b0a06e010f7390be5e52789fd67cf549fca18
Jumbo <jumbo.wjb@hackermail.com>
2015-05-15 00:01:34 +0800

    Create mail.py
    E-mail to add bulk -- cracker

---
+++ mail.py
@@ -0,0 +1,18 @@
+x = open('xingming500.txt','r')
+for f in x:
+	f = f.strip()
+	print f + '@chinabaiker.com'
+	
+	
+	
+	
+zhangnan@chinabaiker.com
+liuguizhen@chinabaiker.com
+liuyu@chinabaiker.com
+liujianjun@chinabaiker.com
+zhangshuying@chinabaiker.com
+lihongxia@chinabaiker.com
+zhaoxiuying@chinabaiker.com
+libo@chinabaiker.com
+wangli@chinabaiker.com
+zhangrong@chinabaiker.com
