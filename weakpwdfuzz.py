c63851cfbf49ed47d4742f1890c8c236074a77b6
Jumbo <jumbo.wjb@hackermail.com>
2016-03-29 16:27:23 +0800

    Update weakpwdfuzz.py

--- weakpwdfuzz.py
+++ weakpwdfuzz.py
@@ -15,6 +15,25 @@ except:
 
 
 
+练习一下def
+import sys
+def weakpwdfuzz():
+    try:
+        key = sys.argv[1]
+        dic = open('dic.txt','r')
+        suc = open('ok.txt','w')
+        for a in dic:
+            a = a.strip()
+            a = a.replace("%username%",key)
+            print a
+            suc.write(a+'\n')
+        dic.close()
+        suc.close()
+    except:
+        print "usage: %s chinabaiker" % sys.argv[0]
+if __name__ == '__main__': 
+    weakpwdfuzz()
+
 
 使用方法
 python weakpwdfuzz.py chinabaiker
