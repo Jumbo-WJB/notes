15a1516dc1d33a0cbc04eb9548092291e1477208
Jumbo <jumbo.wjb@hackermail.com>
2016-08-01 18:33:02 +0800

    Update export_http.py

--- export_http.py
+++ export_http.py
@@ -1,10 +1,5 @@
-yy = open('2.txt','w')
 from libnmap.parser import NmapParser
 nmap_report = NmapParser.parse_fromfile('nmap.xml')
-for xx in [ [(b.service + b.tunnel).replace('sl',''), a.address, b.port] for a in nmap_report.hosts for b in a.services if b.open() and b.service.startswith('http') ]:
-    print xx
-    print>>yy,xx
-    f = open('2.txt','r')
-    a = open('urlok.txt','w')
-    for x in f:
-        print>>a,x.replace("', '","://").replace("', ",":")
+urls = [ (b.service + b.tunnel).replace('sl','') + '://' + a.address + ':' + str(b.port) + '/' for a in nmap_report.hosts for b in a.services if b.open() and b.service.startswith('http') ]
+for x in urls:
+    print x
