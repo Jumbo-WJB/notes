5a969a7cbae09b7c03646a45a11e809ec9af00ab
Jumbo <jumbo.wjb@hackermail.com>
2016-09-23 15:48:54 +0800

    更新正则
    更新正则

--- same-ip-search.py
+++ same-ip-search.py
@@ -15,12 +15,12 @@ try:
 	f = open(zhan + ".txt","w")
 	jieguo = urllib.urlopen(url + str(zhan))
 	content = jieguo.read()
-	ree = r"\<\/span\> \<a href=\'(.*?)\' target=_blank\>"                    //(.*?)非贪婪匹配
+	ree = r"\<a href=\'(.*?)\' target=_blank\>"                    //(.*?)非贪婪匹配
 	ss = re.findall(ree,content)
 	for x in ss:
 		print>>f,x
 	print 'ok,look ' + zhan + '.txt.'
 	f.close()
 except:
-	print 'eg:python pz.py www.chinabaiker.com'
+	print 'eg:python %s www.chinabaiker.com' % sys.argv[0]
  
