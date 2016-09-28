9034f3861b1ac40e77218199e04ea282fe8bee9f
Jumbo <jumbo.wjb@hackermail.com>
2016-09-28 14:53:01 +0800

    Update 任意文件下载.py

--- 任意文件下载.py
+++ 任意文件下载.py
@@ -5,17 +5,15 @@ import urllib
 import requests
 import re
 i = range(100)
-#print i
-url = "http://www.baidu.com:8000/Liems/ExportWord?pkValue=%s" %i
-res = urllib.urlopen(url)
-#print res
-#print res.info()
-aaaa = res.info()
-print aaaa
-filenameRe = re.compile(r'Content-Disposition: filename=\"(.*?)\"')
-filename = re.findall(filenameRe,str(aaaa))
-for x in filename:
-    print x
-    urllib.urlretrieve(url,x)
+for a in i:
+    url = "http://www.baidu.com:8000/Liems/ExportWord?pkValue=%s" %a
+    res = urllib.urlopen(url)
+    aaaa = res.info()
+    print aaaa
+    filenameRe = re.compile(r'Content-Disposition: filename=\"(.*?)\"')
+    filename = re.findall(filenameRe,str(aaaa))
+    for x in filename:
+        print x
+        urllib.urlretrieve(url,x)
 
 //这样就可以根据filename，什么文件就以什么文件名保存在本地。
