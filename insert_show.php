3be627c8e1e831bad611a474deb7159441923393
Jumbo <jumbo.wjb@hackermail.com>
2018-05-17 00:46:37 +0800

    Update insert_show.php

--- insert_show.php
+++ insert_show.php
@@ -1,72 +1,128 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "vuln";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_query($conn, "set character set 'utf8'");
+mysqli_query($conn,"set names 'utf8'");
+// $conn->set_charset("utf8mb4");
+?>
 <html>
 	<head>
-		<title>注册页面</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>漏洞查询</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
 	</head>
 
 	<body>
-		<h1>新用户注册</h1>
+		<h1>漏洞录入</h1>
 		<hr>
 
 		<form method="post" action="">
 			<table>
 				<tr>
-					<td>请输入用户名：<input type="text" name="username"></td>
+					<td>师傅名字：    <input type="text" name="username1"></td>
 				</tr>
 	
 				<tr>
-					<td>请输入first_id：    <input type="text" name="first_id"></td>
+					<td>项目名称：    <input type="text" name="project1"></td>
 				</tr>
-
-
-
+				<tr>
+					<td>高危数量：    <input type="text" name="high1"></td>
+				</tr>
+				<tr>
+					<td>中危数量：    <input type="text" name="medium1"></td>
+				</tr>
+				<tr>
+					<td>低危数量：    <input type="text" name="low1"></td>
+				</tr>
 				<tr>
 					<td><input type="submit" value="提交">
 					
 				</tr>
 			</table>
 		</form>
-	</body>
-</html>
+
 
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "root";
-$dbname = "dvwa";
- 
-// 创建连接
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-} 
-$username = $_POST['username'];
-$first_id = $_POST['first_id'];
-$sql = "insert into users (user_id,first_name,last_name,user,password,avatar,last_login,failed_login) values ('$first_id','$username','jumbo','jumbo','jumbo','jumbo','2016-05-06','0')";
-$result = $conn->query($sql);
+$username1 = $_POST['username1'];
+$project1 = $_POST['project1'];
+$high1 = $_POST['high1'];
+$medium1 = $_POST['medium1'];
+$low1 = $_POST['low1'];
+$sql = "insert into vuln (username,project,high,medium,low) values ('$username1','$project1','$high1','$medium1','$low1')";
+$result =  mysqli_query($conn,$sql)
 ?>
-<html>
-<head>
-  <title>dropdown from mysql</title>
-</head>
-<body>
-  <h1>用户名如下</h1>
-<select>
-<option value=0>--请选择--</option>
+
+
+  <h1>漏洞查询</h1>
+  <hr>
+  根据姓名查询
+  <br>
+<form action="" method="POST">
+<select name="username">
+<option value=0>--用户名--</option>
 <?php
-$sql2 = "SELECT * from users";
-$result2 = $conn->query($sql2);
+$sql2 = "SELECT * from vuln";
+$result2 = mysqli_query($conn,$sql2);
 if ($result2->num_rows > 0) {
     while($row2 = $result2->fetch_assoc()) {
-        echo "<option value='$row2[first_name]'>$row2[first_name]</option>";;
+        echo "<option value='$row2[username]'>$row2[username]</option>";
     }
-} else {
-    echo "0 结果";
 }
-$conn->close();
+
 ?>
+
+
 </select>
-  </form>
+<input type="submit" value="查询">
+</form>
+<br>
+根据项目查询
+<br>
+<form action="" method="POST">
+<select name="project">
+<option value=0>--项目名称--</option>
+<?php
+$result2 = mysqli_query($conn,$sql2);
+if ($result2->num_rows > 0) {
+    while($row2 = $result2->fetch_assoc()) {
+        echo "<option value='$row2[project]'>$row2[project]</option>";
+    }
+}
+
+?>
+
+
+</select>
+<input type="submit" value="查询">
+</form>
+<br>
+
+<?php
+$username = $_POST['username'];
+$project = $_POST['project'];
+$sql3 = "SELECT * from vuln where username = '$username' or project = '$project'";
+$result3 = mysqli_query($conn,$sql3);
+echo "<h1>漏洞提交信息</h1>";
+echo "<hr>"; 
+echo "<table width='700px' border='1px'>";  
+echo "<tr>";
+echo "<th>姓名</th><th>项目名称</th><th>高危数量</th><th>中危数量</th><th>低危数量</th>";
+echo "</tr>";  
+while($row3 = $result3->fetch_assoc()) {
+    echo "<tr>"; 
+    echo "<td>{$row3['username']}</td><td>{$row3['project']}</td><td>{$row3['high']}</td><td>{$row3['medium']}</td><td>{$row3['low']}</td>";
+    echo "</tr>";  
+}
+echo "</table>";  
+?>
 </body>
 </html>
+//CREATE DATABASE vuln;
+//use vuln;
+//create table vuln(username varchar(20),project varchar(100), high int(100), medium int(100), low int(100));
+//desc vuln;
+//insert into users (username,project,high,medium,low) values ('姓名','客户','10','2','20');
+//show create table vuln;
+//alter table vuln convert to character set utf8;      
